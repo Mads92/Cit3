d05474fa386d8123b062b68f57211c0c997b4a42
Mads92 <madskoie@hotmail.com>
2025-10-07 22:27:47 +0200

    Added TcpServer and UrlPArser

---
+++ Assignment3/TcpServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Assignment3;
+
+namespace Assignment3
+{
+    public class TcpServer
+    {
+        private readonly int port = 5000;
+        private readonly RequestValidator validator = new RequestValidator();
+        private readonly UrlParser parser = new UrlParser();
+        private readonly CategoryService categoryService = new CategoryService();
+
+        public void Start()
+        {
+            var listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine($"TCP Server started on port {port}...");
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string requestText = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"Received: {requestText}");
+
+                Request req = parser.ParseUrl(requestText);
+                Response res = validator.ValidateRequest(req);
+
+                string responseText = $"{res.StatusCode} {res.Status} {res.Message}";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(responseText);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Assignment3/UrlParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Assignment3;
+
+namespace Assignment3
+{
+    public class UrlParser
+    {
+        public Request ParseUrl(string url)
+        {
+            var parts = url.Split(' ', 4, StringSplitOptions.RemoveEmptyEntries);
+            var req = new Request();
+
+            if (parts.Length > 0) req.Method = parts[0];
+            if (parts.Length > 1) req.Path = parts[1];
+            if (parts.Length > 2) req.Date = parts[2];
+            if (parts.Length > 3) req.Body = parts[3];
+
+            return req;
+        }
+    }
+}
